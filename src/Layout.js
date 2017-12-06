540753b83be0c80b34977284fd475f098656906c
Artur V. Chernysh <achernysh@tsc.ts>
2017-12-06 18:49:01 +0300

    nothing

--- src/Layout.js
+++ src/Layout.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
+import { Grid, GridRow, GridColumn, Icon } from 'semantic-ui-react';
 
 export default class Layout extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menuOpen: false
+    };
+
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState({
+      menuOpen: !this.state.menuOpen
+    });
+  }
+
   render() {
     return (
       <header className="page-header">
-        <div className="ui container">
-          <div class="ui equal width grid">
-            <div class="column">
-              <div class="ui segment">1</div>
-            </div>
-            <div class="eight wide column">
-              <div class="ui segment">2</div>
-            </div>
-            <div class="column">
-              <div class="ui segment">3</div>
-            </div>
-          </div>
-        </div>
+        <Grid container={ true } columns={ 2 }>
+          <GridRow style={{ paddingBottom: '0' }}>
+
+            <GridColumn mobile={ 10 }>
+              <Icon className="page-header__logo" name='umbrella'/>
+            </GridColumn>
+
+            <GridColumn verticalAlign={ 'middle' }>
+              <nav className={ `page-header__nav ${this.state.menuOpen ? 'menu-opened' : 'menu-closed'}` }>
+                <ul>
+                  <li><a href="/users">Users</a></li>
+                  <li><a href="/articles">Articles</a></li>
+                  <li className="page-header__menu-collapse" onClick={ this.toggleMenu }>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                  </li>
+                </ul>
+              </nav>
+            </GridColumn>
+
+          </GridRow>
+        </Grid>
       </header>
     );
   }
