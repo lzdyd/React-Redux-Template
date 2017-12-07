1bdb5f4f2cc7b0f64658928b864b715fef0de4a1
Artur V. Chernysh <achernysh@tsc.ts>
2017-12-07 15:10:48 +0300

    cssChanges

--- src/Layout.js
+++ src/Layout.js
@@ -20,36 +20,50 @@ export default class Layout extends Component {
 
   render() {
     return (
-      <header className={ `page-header ${this.state.menuOpen ? 'menu-opened' : 'menu-closed'}` }>
-        <Grid container={ true } columns={ 3 }>
-          <GridRow style={{ paddingBottom: '0' }}>
+      <div className="main-app">
+{/*        <header className={ `page-header ${this.state.menuOpen ? 'menu-opened' : 'menu-closed'}` }>
+          <Grid container={ true } columns={ 3 } style={{ marginBottom: '0' }}>
+            <GridRow style={{ paddingBottom: '0' }}>
 
-            <GridColumn mobile={ 10 } computer={ 2 } style={{ outline: '2px solid red' }}>
-              <Icon className="page-header__logo" name='umbrella'/>
-            </GridColumn>
+              <GridColumn mobile={ 10 } computer={ 10 } tablet={ 8 }>
+                <Icon className="page-header__logo" name='umbrella'/>
+              </GridColumn>
 
-            <GridColumn verticalAlign={ 'middle' } floated={ 'right' } only={ 'mobile' } className="page-header__menu-col">
-              <div className="page-header__menu-collapse" onClick={ this.toggleMenu }>
-                <span></span>
-                <span></span>
-                <span></span>
-              </div>
-            </GridColumn>
+              <GridColumn verticalAlign={ 'middle' } floated={ 'right' } only={ 'mobile' } className="page-header__menu-col">
+                <div className="page-header__menu-collapse" onClick={ this.toggleMenu }>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+              </GridColumn>
 
-            <GridColumn mobile={ 10 } computer={ 4 }>
-              <nav className='page-header__nav'>
-                <ul>
-                  <li><a href="/users">Users</a></li>
-                  <li><a href="/articles">Articles</a></li>
-                  <li>
-                    <Button>Log in</Button>
-                  </li>
-                </ul>
-              </nav>
+              <GridColumn mobile={ 10 } computer={ 6 } tablet={ 8 } className="page-header__nav-col">
+                <nav className='page-header__nav'>
+                  <ul>
+                    <li><a href="/users">Users</a></li>
+                    <li><a href="/articles">Articles</a></li>
+                    <li className="auth_btn">
+                      <Button color={ 'facebook' }>Log in</Button>
+                    </li>
+                  </ul>
+                </nav>
+              </GridColumn>
+            </GridRow>
+          </Grid>
+        </header>
+
+        <footer className="page-footer">
+          <Grid container={ true }>
+            <GridColumn style={{ paddingTop: '0' }}>
+              <h1>test</h1>
             </GridColumn>
-          </GridRow>
-        </Grid>
-      </header>
+          </Grid>
+        </footer>*/}
+
+        <header className={ `page-header ${this.state.menuOpen ? 'menu-opened' : 'menu-closed'}` }>
+
+        </header>
+      </div>
     );
   }
 }
